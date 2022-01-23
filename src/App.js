64ef4fd708f2f7fa7a37a01c985353e11b33f442
github username <altmanguitar@gmail.com>
2022-01-23 01:01:54 -0500

    commit tree 4ec4676f44a3f8bb5e3a0bc9d152ccc6f790a8bb

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchCats } from './actions/catActions';
+import CatList from './CatList'
 
 class App extends Component {   
   
+  componentDidMount() {
+    console.log(this.props);
+    this.props.fetchCats();
+  }
+
+  // renderLoadingMessage = () => {
+  //   return <h4>Loading...</h4>
+  // }
+
   render() {
+    console.log(this.props.catPics)
     return (
-      <div>
+      <div className='App'>
         <h1>CatBook</h1>
         {/* add CatList component here */}
+        < CatList catPics={this.props.catPics} />
+
+        {/* { this.props.catPics.loading ?  
+            this.renderLoadingMessage() : <CatList catPics={this.props.catPics} /> 
+        } */}
+
       </div>
     );
   }
 }
 
-export default App
+const mapsStateToProps = (state) => {
+  return {
+    catPics: state.cats, // becomes props in app component
+    loading: state.loading,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchCats: () => dispatch(fetchCats()),
+  };
+};
+
+export default connect(mapsStateToProps, mapDispatchToProps)(App)
 
--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,45 @@
 // write your CatList component here
+import React from 'react';
+
+const CatList = (props) => {
+
+    const listCats = props.catPics.map((pic) => <img key={pic.id} src={pic.url} alt="cat pic"/>)
+
+    return (
+        <div>
+            {listCats}
+        </div>);
+};
+
+export default CatList;
+
+// ALTERNATE 1:
+// const CatList = (props) => {
+//     function listCats () {
+//       return props.catPics.map(catPic => {
+//         return (
+//           <img src={catPic.url} role='presentation' />
+//         )
+//       })
+//     }
+//     return (
+//       <div>
+//         { listCats() }
+//       </div>
+//     )
+//   }
+
+// ALTERNATE 2:
+// class CatList extends Component {
+//     renderCatPics = () => {
+//        return this.props.catPics.map(cat => {
+//           return <img key={cat.id} src={cat.url} alt="Picture of a cat" />
+//        });
+//     }
+   
+//     render() {
+//        return (
+//           <div>{this.renderCatPics()}</div>
+//        )
+//     }
+//  }
